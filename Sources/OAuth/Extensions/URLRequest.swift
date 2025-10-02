85104c28866fa1fcf23f91f7b50d8477b7d90df6
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-10-01 23:36:36 -0400

    Removed deprecated code

--- Sources/OAuth/Extensions/URLRequest.swift
+++ Sources/OAuth/Extensions/URLRequest.swift
@@ -21,29 +21,3 @@ internal extension URLRequest {
         self.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
     }
 }
-
-internal extension URLClient {
-    
-    @discardableResult
-    func request(
-        _ urlRequest: URLRequest,
-        decoder: JSONDecoder = .oauth,
-        statusCode: inout Int
-    ) async throws -> Data {
-        let (data, urlResponse) = try await self.data(for: urlRequest)
-        guard let httpResponse = urlResponse as? HTTPURLResponse else {
-            assertionFailure("Invalid response type \(urlResponse)")
-            throw Foundation.URLError(.unknown)
-        }
-        let expectedStatusCode = statusCode
-        statusCode = httpResponse.statusCode
-        guard statusCode == expectedStatusCode else {
-            if data.isEmpty == false, let decodedResponse = try? decoder.decode(OAuthErrorResponse.self, from: data) {
-                throw OAuthError.errorResponse(decodedResponse)
-            } else {
-                throw OAuthError.invalidStatusCode(httpResponse.statusCode)
-            }
-        }
-        return data
-    }
-}
