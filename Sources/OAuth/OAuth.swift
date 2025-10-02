0abc2fe818487c7f5e1b0266cfa4f1bc2d6fa11a
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-10-01 21:03:18 -0400

    Add umbrella import

---
+++ Sources/OAuth/OAuth.swift
@@ -0,0 +1,11 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+import HTTPTypes
+#if canImport(HTTPTypesFoundation)
+import HTTPTypesFoundation
+#endif
+import HTTP
+
+
